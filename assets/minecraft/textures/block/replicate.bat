0f11b7cfc9ded5d1c6d2b2a25ca1dbce876c4b71
src3453 <info@example.com>
2023-06-29 01:38:54 +0900

    upscaled detector

--- assets/minecraft/textures/block/replicate.bat
+++ assets/minecraft/textures/block/replicate.bat
@@ -1,4 +1,8 @@
 @echo off
 for /l %%n in (0,1,15) do (
-copy .\target.png .\target_%%n.png
+copy .\daylight_detector_top.png .\daylight_detector_top%%n.png 
+)
+@echo off
+for /l %%n in (0,1,15) do (
+copy .\daylight_detector_inverted_top.png .\daylight_detector_inverted_top%%n.png 
 )
